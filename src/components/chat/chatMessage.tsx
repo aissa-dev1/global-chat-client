ad1dfce9f42d90fe57adbf4df3ab1c731404f5e6
aissa-dev1 <aissabedr74@gmail.com>
2024-07-30 19:41:12 -0700

    remove: glitched time ago logic and replace it with simple one

--- src/components/chat/chatMessage.tsx
+++ src/components/chat/chatMessage.tsx
@@ -1,7 +1,6 @@
-import { createSignal, onCleanup, onMount } from "solid-js";
 import { ChatMessageType } from "../../features/chat";
 import { store } from "../../store";
-import { timeAgo } from "../../utils/time";
+import { formatDate, formatTime } from "../../utils/time";
 
 export default function ChatMessage({
   userId,
@@ -9,20 +8,6 @@ export default function ChatMessage({
   content,
   createdAt,
 }: ChatMessageType) {
-  let createdAtTextInterval!: number;
-  let timeAgoDuration = 5000;
-  const [createdAtText, setCreatedAtText] = createSignal(timeAgo(createdAt));
-
-  onMount(() => {
-    createdAtTextInterval = setInterval(() => {
-      setCreatedAtText(timeAgo(createdAt));
-    }, timeAgoDuration);
-  });
-
-  onCleanup(() => {
-    clearInterval(createdAtTextInterval);
-  });
-
   return (
     <div class="flex flex-col gap-2 bg-white text-black p-4 rounded-lg">
       <div class="flex items-center justify-between">
@@ -34,7 +19,11 @@ export default function ChatMessage({
         >
           {username}
         </h3>
-        <span class="text-sm shrink-0 text-slate-700">{createdAtText()}</span>
+        <div class="flex flex-col gap-2">
+          <span class="text-sm shrink-0 text-slate-700">
+            {formatDate(createdAt)} | {formatTime(createdAt)}
+          </span>
+        </div>
       </div>
       <p class="text-sm font-medium">{content}</p>
     </div>
