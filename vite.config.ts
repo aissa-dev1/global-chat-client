69df5aa27101163fe7bcbccfac4e28670b4bdfe6
aissa-dev1 <aissabedr74@gmail.com>
2024-07-30 19:40:14 -0700

    add: local network server for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
-})
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 3000, // You can change this to any port number you prefer
+  },
+});
